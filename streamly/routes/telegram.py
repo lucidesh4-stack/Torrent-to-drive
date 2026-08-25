72559ebe310f45fe8923804e033210733e8471f8
lucidesh4-stack <lucidesh4@gmail.com>
2026-08-25 22:17:23 +0530

    fix(telegram): import get_bot_id and post_file_id_to_channel in telegram.py

--- streamly/routes/telegram.py
+++ streamly/routes/telegram.py
@@ -32,7 +32,7 @@ from ..security import (
 )
 from ..core.http_client import SeedrDownloader
 from .telegram_client import manager as tg_manager, safe_disconnect, get_telegram_client
-from .telegram_bot_api_daemon import upload_via_local_bot_api
+from .telegram_bot_api_daemon import upload_via_local_bot_api, get_bot_id, post_file_id_to_channel
 from ..cloud_service import format_size
 
 log = logging.getLogger(__name__)
